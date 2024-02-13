e311d01d76d01efc380ea7036932515ed1b80137
Alexander <clawsonx@udel.edu>
2024-02-13 15:37:25 -0500

    Task 3 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,11 +8,45 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <body>Hello World</body>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <h1>New Header</h1>
+            <div>
+                <img src="" alt="Alt text for image" />
+            </div>
+            <div>
+                List of Elements:
+                <ul>
+                    <li>List Element 1</li>
+                    <li>List Element 2</li>
+                    <li>List Element 3</li>
+                </ul>
+            </div>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>Column 1</Col>
+                        <div
+                            style={{
+                                width: "200px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <Col>Column 2</Col>
+                        <div
+                            style={{
+                                width: "200px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Row>
+                </Container>
+            </div>
             <p>Name: Alexander Clawson</p>
         </div>
     );
