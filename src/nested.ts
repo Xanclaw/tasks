7b83ca2edf02b7a365702ee79e40966bcd9476e3
Alexander <clawsonx@udel.edu>
2024-02-27 14:22:05 -0500

    function 7 completed

--- src/nested.ts
+++ src/nested.ts
@@ -64,7 +64,9 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions
+        .filter((question) => question.published === true)
+        .reduce((totalPoints, question) => totalPoints + question.points, 0);
 }
 
 /***
